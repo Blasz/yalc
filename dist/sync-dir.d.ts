ad196600830bb2458ae9fb56c1d17f1122738d61
Michael Blaszczyk <mblaszczyk@atlassian.com>
2019-10-16 11:19:03 +1100

    Move compiled files to dist to workaround typing issues
    The existence of .ts files in src causes problems for consumers

---
+++ dist/sync-dir.d.ts
@@ -0,0 +1,15 @@
+/// <reference types="node" />
+import * as fs from 'fs-extra';
+declare type Cache = {
+    [dir: string]: {
+        glob: string[];
+        files: {
+            [file: string]: {
+                stat: fs.Stats;
+                hash: string;
+            };
+        };
+    };
+};
+export declare const copyDirSafe: (srcDir: string, destDir: string, cache?: Cache) => Promise<void>;
+export {};
